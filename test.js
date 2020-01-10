a7b6c75468c7543bb77a3689fe124694829905d1
Matt Derosier <derosier.matt@gmail.com>
2020-01-10 13:13:42 -0600

    Refactored, reduced size by 33%

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 /** Todo: 
- *      You got this fam
+ *      1: Refactor size/placement values - values should be placed in a variable section before the generator portion for ease of redefinition / future possible sizing options
+ *      2: Redo sizing & placement options to reflect a new smaller default size
+ *      3: Add new site for C.F., change "Schools" to "Site"
  */
 function cellMaker() {
     var cellBox = document.getElementById("cellcheck");
@@ -81,6 +83,13 @@ function draw() {
         var superSocial = 0;
     }
 
+    if (twitterid != ''){
+        twitterid = '@' + twitterid;
+    }
+    else {
+        
+    }
+
     var longname = '0';
     if (name.length > 26) {
         longname = '1';
@@ -101,18 +110,21 @@ function draw() {
         switch (schoolid) {                                         //Constructs the school object
             case 'WHS':
                 school.address = '4200 32nd St W PO BOX 1407';
+                school.address2 = 'Williston, ND 58802';
                 school.phone = '701-572-0967 ' + extension;
                 school.logo = document.getElementById('whslogo');
                 school.name = 'Williston High School';
                 break;
             case 'Bakken':
                 school.address = '502 W Highland Dr PO BOX 1407';
+                school.address2 = 'Williston, ND 58802';
                 school.phone = '701-713-7300 ' + extension;
                 school.logo = document.getElementById('bakkenlogo');
                 school.name = 'Bakken Elementary'
                 break;
             case 'DelEast':
                 school.address = '2304 11th Ave W PO BOX 1407';
+                school.address2 = 'Williston, ND 58802';
                 school.phone = '701-572-1055 ' + extension;
                 school.logo = document.getElementById('whslogo');
                 school.name  = 'Del Easton Alternative High School';
@@ -170,237 +182,136 @@ function draw() {
                 school.phone = 'ERROR ' + extension; 
                 school.logo = 'ERROR';
         }
+        //IF/ELSE for social stuff should go HERE so I can define item & logo
+
+
+        //    Sizing section starts here
+        var defaultsize = {
+            // fonts
+            namefont1: '32px arcon' ,
+            namefont2: '20px arcon' ,
+            titlefont: '13px arcon' ,
+            sitenamefont: '20px lato' , 
+            addressfont: '14px lato' ,
+            twitfont: '14px lato' ,
+            // end fonts
+            // elements
+            bgsize: {x:0, y:0, h:600, v:133},
+            wpsdsize: {p:wpsdlogo, x:0, y:0, h:133, v:133},
+            vertdivsize: {x:134, y:0, h:2, v:133},
+            nameplate: {x:134, y:0, h:800, v:40},
+            hordivsize: {x:134, y:40, h:800, v:20},
+            infoboxsize: {x:135, y:0, h:700, v:82},
+            unamesize: {p:name, x:145, y:35},
+            unamesize2: {p:name, x:145, y:20},
+            titlesize: {p:jobtitle, x:148, y:55},
+            sitenamesize: {p:school.name, x:148, y:78},
+            add1size: {p:school.address, x:148, y:92},
+            add2size: {p:school.address2, x:148, y:109},
+            phonesize: {p:school.phone, x:148, y:127},
+            cellsize: {p:cellnumber, x:396, y:127},
+            sitelogo: {p:school.logo, x:525, y:66, h:66, v:66},
+            
+            resizefactor: {a: 12,b: 14,},
+            social1: {/*p:s1item, l:s1logo,*/ x:396, y:127, h:14, v:14, o:12},
+            social2: {/*p:s2item, l:s2logo,*/ x:396, y:109, h:14, v:14, o:12},
+            Social3: {/*p:s3item, l:s3logo,*/ x:396, y:92,  h:14, v:14, o:12},
+        };
+        
+
+            // Eventually below this line there will be an if/else or switch
+            // Size selection
+        var size = defaultsize;
+        //    Sizing section ends here
     
-//    switch (typeselect) {
-        
-/*        case 'style1':
-            if (canvas.getContext) {
-                //background
-                var ctx = canvas.getContext('2d');
-                ctx.fillStyle = colors.white;
-                ctx.fillRect(0, 0, 900, 200);
 
-                //logo
-                var ctz = canvas.getContext('2d');
-                ctz.drawImage (wpsdlogo, 0, 0, 200, 200);
-
-                //horizontal bg element
-                var horizbar = canvas.getContext('2d');
-                horizbar.fillStyle = colors.black;
-                horizbar.fillRect(202, 0, 700, 82);
-                                                                
-                //horizontal job title background
-                var hbar2 = canvas.getContext('2d');
-                hbar2.fillStyle = colors.ltgray;
-                hbar2.fillRect(410, 62, 500, 30);
-
-                //Vertical separator
-                var vsep = canvas.getContext('2d');
-                vsep.fillStyle = colors.orange; 
-                vsep.fillRect(201, 0, 2, 200);
-
-                //school logo
-                var slogo = canvas.getContext('2d');
-                slogo.drawImage (school.logo, 795, 100, 100, 100);
-
-                //Name
-                var cty = canvas.getContext('2d');
-                cty.fillStyle = colors.white;
-                if (longname == '0') {
-                    cty.font = '48px arcon';
-                    cty.fillText(name, 220, 53);
-                }
-                else {
-                    cty.font = '30px arcon'
-                    cty.fillText(name, 220, 43);
-                }
-                
-
-                //Job title
-                var puttitle = canvas.getContext('2d');
-                puttitle.fillStyle = colors.black;
-                puttitle.font = '18px arcon';
-                puttitle.fillText(jobtitle, 420, 83);
-
-                //School name
-                var sname = canvas.getContext('2d');
-                sname.fillStyle = colors.black;
-                sname.font = '30px arcon';
-                sname.fillText(school.name, 225, 115);
-
-                //Address and phone number
-                var addressphone = canvas.getContext('2d');
-                addressphone.fillStyle = colors.black;
-                addressphone.font = '22px arcon';
-                addressphone.fillText(school.address, 225, 140);
-                addressphone.fillText('Williston, ND 58802', 225, 165)
-                addressphone.fillText(school.phone, 225, 192);
-        
-            }
-            break; 
-*/			
-/*         case 'style2':
-            if (canvas.getContext) {
-                //background
-                var ctx = canvas.getContext('2d');
-                ctx.fillStyle = colors.white;
-                ctx.fillRect(0, 0, 900, 200);
-
-                //logo
-                //var ctz = canvas.getContext('2d');
-                //ctz.drawImage (wpsdlogo, 0, 0, 200, 200);
-
-                //horizontal bg element
-                var horizbar = canvas.getContext('2d');
-                horizbar.fillStyle = colors.ltgray;
-                horizbar.fillRect(0, 0, 900, 62);
-
-                //horizontal job title background
-                //var hbar2 = canvas.getContext('2d');
-                //hbar2.fillStyle = colors.black;
-                //hbar2.fillRect(410, 62, 500, 30);
-
-                //Vertical separator
-                var vsep = canvas.getContext('2d');
-                vsep.fillStyle = colors.orange; 
-                vsep.fillRect(449, 81, 2, 100);
-
-                //school logo
-                var slogo = canvas.getContext('2d');
-                if (xlongname == '0'){
-                    slogo.drawImage (school.logo, 334, 81, 100, 100);
-                }
-                else {
-                    slogo.drawImage (school.logo, 350, 100, 80, 80);
-                }
-
-                //Top text
-                var distname = canvas.getContext('2d');
-                distname.fillStyle = colors.black;
-                distname.font = '40px arcon';
-                distname.fillText ('WILLISTON PUBLIC SCHOOL DISTRICT #1', 75, 45);
-
-                //Name
-                var cty = canvas.getContext('2d');
-                cty.fillStyle = colors.black;
-                if (longname == '0') {
-                    cty.font = '32px arcon';
-                    cty.fillText(name, 15, 100);
-                }
-                else {
-                    cty.font = '25px arcon';
-                    cty.fillText(name, 15, 100);
-                }
-
-                //Job title
-                var puttitle = canvas.getContext('2d');
-                puttitle.fillStyle = colors.black;
-                puttitle.font = '18px arcon';
-                if (longname == '0') {
-                    puttitle.fillText(jobtitle, 15, 130);
-                }
-                else {
-                    puttitle.fillText(jobtitle, 15, 125);
-                }
-
-                //School name
-                var schoolname = canvas.getContext('2d');
-                schoolname.fillStyle = colors.black;
-                schoolname.font ='30px arcon';
-                schoolname.fillText(school.name, 476, 100);
-
-                //Address and phone number
-                var addressphone = canvas.getContext('2d');
-                addressphone.fillStyle = colors.black;
-                addressphone.font = '25px arcon';
-                addressphone.fillText(school.address, 476, 138);
-                addressphone.fillText('Williston, ND 58802', 476, 173)
-                addressphone.fillText(school.phone, 15, 170);
-            }
-            break;
-			
-        case 'style3': */
             if (canvas.getContext) {   
                 //background
                 var ctx = canvas.getContext('2d');
                 ctx.fillStyle = colors.ltgray;
-                ctx.fillRect(0, 0, 900, 200);
+                ctx.fillRect(size.bgsize.x, size.bgsize.y, size.bgsize.h, size.bgsize.v,);
 
                 //logo
                 var ctz = canvas.getContext('2d');
-                ctz.drawImage (wpsdlogo, 0, 0, 200, 200);
-
+                ctz.drawImage(size.wpsdsize.p, size.wpsdsize.x, size.wpsdsize.y, size.wpsdsize.h, size.wpsdsize.v);
+                
                 //horizontal bg element
                 var horizbar = canvas.getContext('2d');
                 horizbar.fillStyle = colors.white;
-                horizbar.fillRect(202, 0, 700, 82);
+                horizbar.fillRect(size.nameplate.x, size.nameplate.y, size.nameplate.h, size.nameplate.v);
 
                 //horizontal job title background
                 var hbar2 = canvas.getContext('2d');
                 hbar2.fillStyle = colors.black;
-                hbar2.fillRect(201, 61, 800, 30);
+                hbar2.fillRect(size.hordivsize.x, size.hordivsize.y, size.hordivsize.h, size.hordivsize.v);
 
                 //Vertical separator
                 var vsep = canvas.getContext('2d');
                 vsep.fillStyle = colors.black; 
-                vsep.fillRect(201, 0, 2, 200);
+                vsep.fillRect(size.vertdivsize.x, size.vertdivsize.y, size.vertdivsize.h, size.vertdivsize.v);
 
                 //school logo
                 var slogo = canvas.getContext('2d');
-                slogo.drawImage(school.logo, 795, 100, 100, 100);
+                slogo.drawImage(size.sitelogo.p, size.sitelogo.x, size.sitelogo.y, size.sitelogo.h, size.sitelogo.v);
 
                 //Name
                 var cty = canvas.getContext('2d');
                 cty.fillStyle = colors.orange;
                 if (longname == '0') {
-                    cty.font = '48px arcon';
-                    cty.fillText(name, 220, 53);
+                    cty.font = size.namefont1;
+                    cty.fillText(size.unamesize.p, size.unamesize.x, size.unamesize.y);
                 }
                 else {
-                    cty.font = '30px arcon'
-                    cty.fillText(name, 220, 43);
+                    cty.font = size.namefont2;
+                    cty.fillText(size.unamesize2.p, size.unamesize2.x, size.unamesize2.y);
                 }
 
                 //Job title
                 var puttitle = canvas.getContext('2d');
                 puttitle.fillStyle = colors.white;
-                puttitle.font = '18px arcon';
-                puttitle.fillText(jobtitle, 225, 83);
+                puttitle.font = size.titlefont;
+                puttitle.fillText(size.titlesize.p, size.titlesize.x, size.titlesize.y);
 
                 //School name
                 var sname = canvas.getContext('2d');
                 sname.fillStyle = colors.black;
-                sname.font = '30px lato';
-                sname.fillText(school.name, 225, 115);
+                sname.font = size.sitenamefont;
+                sname.fillText(size.sitenamesize.p, size.sitenamesize.x, size.sitenamesize.y);
 
                 //Address and phone number
                 var addressphone = canvas.getContext('2d');
                 addressphone.fillStyle = colors.black;
-                addressphone.font = '22px lato';
-                addressphone.fillText(school.address, 225, 140);
-                addressphone.fillText('Williston, ND 58802', 225, 165)
-                addressphone.fillText(school.phone, 225, 192);
+                addressphone.font = size.addressfont;
+                addressphone.fillText(size.add1size.p, size.add1size.x, size.add1size.y);
+                addressphone.fillText(size.add2size.p, size.add2size.x, size.add2size.y);
+                addressphone.fillText(size.phonesize.p, size.phonesize.x, size.phonesize.y);
 
-                //cell phone number
+                //Here's where the social parts get added
+
+
+                /*/cell phone number
+                if (cellexists == 1) {
                 var cell = canvas.getContext('2d');
                 cell.fillStyle = colors.black;
-                cell.font = '22px lato';
-                cell.fillText(cellnumber, 600, 192);
+                cell.font = size.addressfont;
+                cell.fillText(size.cellsize.p, size.cellsize.x, size.cellsize.y); 
+                } */
 
                 //Twitter & ig
                 
+                /*
                 var twit = canvas.getContext('2d');
                 twit.fillStyle = colors.black
-                twit.font = '22px lato';
+                twit.font = size.twitfont;
                 if (superSocial == 1) {
                     if (cellexists == 1) {
-                        var twitY = 140;
-                        var igY = 165;
+                        var twitY = 92;
+                        var igY = 109;
                     }
                     else {
-                        var twitY = 165;
-                        var igY = 192;
-
+                        var twitY = 109;
+                        var igY = 127;
+        
                     }
                     twit.fillText(twitterid, 625, twitY);
                     twit.drawImage(twitlogo, 600, twitY - 18, 22, 22);
@@ -427,78 +338,8 @@ function draw() {
 
                     }
 
-                }
+                } */
 
-
-                //Instagram
 			}
-/*			break;
-			
-
-        default:
-            if (canvas.getContext) {
-                //background
-                var ctx = canvas.getContext('2d');
-                ctx.fillStyle = colors.ltgray;
-                ctx.fillRect(0, 0, 900, 200);
-
-                //logo
-                var ctz = canvas.getContext('2d');
-                ctz.drawImage (wpsdlogo, 0, 0, 200, 200);
-
-                //horizontal bg element
-                var horizbar = canvas.getContext('2d');
-                horizbar.fillStyle = colors.white;
-                horizbar.fillRect(202, 0, 700, 82);
-
-                //horizontal job title background
-                var hbar2 = canvas.getContext('2d');
-                hbar2.fillStyle = colors.black;
-                hbar2.fillRect(201, 61, 800, 30);
-
-                //Vertical separator
-                var vsep = canvas.getContext('2d');
-                vsep.fillStyle = colors.black; 
-                vsep.fillRect(201, 0, 2, 200);
-
-                //school logo
-                var slogo = canvas.getContext('2d');
-                slogo.drawImage (school.logo, 795, 100, 100, 100);
-
-                //Name
-                var cty = canvas.getContext('2d');
-                cty.fillStyle = colors.orange;
-                if (longname == '0') {
-                    cty.font = '48px arcon';
-                    cty.fillText(name, 220, 53);
-                }
-                else {
-                    cty.font = '30px arcon'
-                    cty.fillText(name, 220, 43);
-                }
-
-                //Job title
-                var puttitle = canvas.getContext('2d');
-                puttitle.fillStyle = colors.white;
-                puttitle.font = '18px arcon';
-                puttitle.fillText(jobtitle, 225, 83);
-
-                //School name
-                var sname = canvas.getContext('2d');
-                sname.fillStyle = colors.black;
-                sname.font = '30px arcon';
-                sname.fillText(school.name, 225, 115);
-
-                //Address and phone number
-                var addressphone = canvas.getContext('2d');
-                addressphone.fillStyle = colors.black;
-                addressphone.font = '22px arcon';
-                addressphone.fillText(school.address, 225, 140);
-                addressphone.fillText('Williston, ND 58802', 225, 165)
-                addressphone.fillText(school.phone, 225, 192);
-        
-            } 
-
-    } */
     
 }
